a4515926938fd4d1e166a78da3844397c689fa86
Noah H. Smith <noah@batteredoats.com>
2016-10-16 13:27:35 -0700

    reogranizing for multiple game modes

--- src/App.vue
+++ src/App.vue
@@ -1,155 +1,22 @@
-<template>
-  <div>
-    <span class='right'>{{flagCount}}</span>
-    :
-    <timer class="left" :time="time"></timer>
-    :
-    <a href="" @click.prevent="restart">restart</a>
-    <template v-for="(row, r) in matrix">
-      <div class="row">
-        <div class="col" v-for="(el, c) in row">
-          <tile
-            :isBomb="el.isBomb"
-            :isMasked="el.isMasked"
-            :isFlagged="el.isFlagged"
-            :bombCount="neighboringBombs(matrix, r, c)"
-            @flag="flag(r, c)"
-            @unmaskAroundFlags="unmaskAroundFlags(r, c)"
-            @unmask="unmask(r, c)">
-          </tile>
-        </div>
-      </div>
-    </template>
-  </div>
+<template lang="html">
+  <div :is="gameMode"></div>
 </template>
 
 <script>
-import {
-  countFlags,
-  findBombs,
-  initializeMap,
-  isComplete,
-  isPlayable,
-  neighboringBombs,
-  toggleFlag,
-  unmask,
-  unmaskAroundFlags,
-  validFirstPlay,
-} from '../lib/gameplay';
-import Tile from './components/Tile.vue'
-import Timer from './components/Timer.vue'
+import Classic from './classic/App.vue'
 
 export default {
   components: {
-    Tile,
-    Timer
+    Classic
   },
 
   data() {
     return {
-      gameSize: [30, 16],
-      bombCount: 99,
-      matrix: [],
-      playing: true,
-      startedAt: 0,
-      time: 0,
+      gameMode: 'classic'
     }
-  },
-
-  mounted() {
-    this.matrix = initializeMap(this.gameSize[0], this.gameSize[1], this.bombCount)
-  },
-
-  watch: {
-    startedAt() {
-      const setTimer = () => {
-        window.requestIdleCallback(() => {
-          if (this.playing) {
-            this.time = Date.now() - this.startedAt
-            setTimer()
-          }
-        })
-      }
-
-      setTimer()
-    },
-
-    matrix: {
-      handler() {
-        if (this.playing && isComplete(this.matrix) || !isPlayable(this.matrix)) {
-          this.stop()
-        }
-
-
-        if (!isComplete(this.matrix) && !isPlayable(this.matrix)) {
-          const unmasked = findBombs(this.matrix)
-          unmasked.forEach((p) => this.matrix[p.r][p.c].isMasked = false)
-        }
-      },
-      deep: true
-    }
-  },
-
-  computed: {
-    flagCount() {
-      return this.bombCount - countFlags(this.matrix)
-    }
-  },
-
-  methods: {
-    neighboringBombs,
-
-    start(r, c) {
-      while (!validFirstPlay(this.matrix, r, c)) {
-        this.matrix = initializeMap(this.gameSize[0], this.gameSize[1], this.bombCount)
-      }
-
-      this.playing = true
-      this.startedAt = Date.now()
-    },
-
-    restart() {
-      this.matrix = initializeMap(this.gameSize[0], this.gameSize[1], this.bombCount)
-      this.playing = false
-      this.startedAt = 0
-      this.time = 0
-    },
-
-    stop() {
-      this.playing = false
-    },
-
-    flag(r, c) {
-      if (!isPlayable(this.matrix)) return
-      if (!this.startedAt) this.start(r, c)
-      toggleFlag(this.matrix, r, c)
-    },
-
-    unmaskAroundFlags(r, c) {
-      if (!isPlayable(this.matrix)) return
-      const unmasked = unmaskAroundFlags(this.matrix, r, c)
-      unmasked.forEach((p) => this.matrix[p.r][p.c].isMasked = false)
-    },
-
-    unmask(r, c, $event) {
-      if (!isPlayable(this.matrix)) return
-      if (!this.startedAt) this.start(r, c)
-      const unmasked = unmask(this.matrix, r, c)
-      unmasked.forEach((p) => this.matrix[p.r][p.c].isMasked = false)
-    },
   }
 }
 </script>
 
-<style>
-body {
-  font-family: -apple-system, sans-serif;
-  background: white;
-  text-align: center;
-}
-
-.col {
-  display: inline-block;
-  vertical-align: middle;
-}
+<style lang="css">
 </style>
