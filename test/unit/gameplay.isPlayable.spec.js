7f4c6db1de3f5c96b4406e1e5bd3baf7607e9826
Noah H. Smith <noah@batteredoats.com>
2016-10-16 21:00:34 -0700

    adding another test case, for the sake of it

--- test/unit/gameplay.isPlayable.spec.js
+++ test/unit/gameplay.isPlayable.spec.js
@@ -5,6 +5,7 @@ describe('isPlayable', function () {
   const o = { isMasked: false, isFlagged: false, isBomb: false }
   const f = { isMasked: true, isFlagged: true, isBomb: false }
   const b = { isMasked: true, isFlagged: false, isBomb: true }
+  const B = { isMasked: false, isFlagged: true, isBomb: true }
   const x = { isMasked: false, isFlagged: false, isBomb: true }
 
   it('returns true if no bombs are unmasked', function () {
@@ -15,6 +16,16 @@ describe('isPlayable', function () {
     ]
 
     expect(isPlayable(matrix)).toBe(true)
+  })
+
+  it('returns true if no bombs are unmasked or unflagged', function () {
+    const matrix = [
+      [o, o, B],
+      [b, o, o],
+      [o, o, o]
+    ]
+
+    expect(isPlayable(matrix)).toBe(false)
   })
 
   it('returns false if a bomb is exposed', function () {
