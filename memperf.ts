bb1465b59db16e4ee0d5f7f7513de12400e25f0f
Noah Smith <noah@batteredoats.com>
2019-08-21 19:37:11 -0400

    introducing memperf for memory performance testing

---
+++ memperf.ts
@@ -0,0 +1,39 @@
+import { times } from 'lodash'
+import { initializeMap } from './lib/gameplay'
+import { performance } from 'perf_hooks'
+
+const beforeAll = process.memoryUsage().heapUsed
+
+const maxSize = 2048
+const maxMem = 262144 // 256MB
+const runCount = 5
+
+console.log(`size, bomb count, ${times(runCount, Number).map(i => `m${i+1}`).join(', ')}, time`);
+
+for (let i=1; i <= maxSize; i++) {
+  const size = i
+  const memory = new Array(runCount)
+  const bc = Math.floor(size * .2)
+  const timeStart = performance.now()
+
+  for (let j=0; j < runCount; j++) {
+    const before = process.memoryUsage().heapUsed
+    const map = initializeMap(size, size, bc)
+    const after = process.memoryUsage().heapUsed
+
+    const diff = after - before
+
+    memory[j] = Math.round(diff / 1024)
+
+    if (diff > maxMem) {
+      break;
+    }
+  }
+
+  const timeEnd = performance.now()
+  const timeDiff = timeEnd - timeStart
+  console.log(`${size}, ${bc}, ${memory.join(', ')}, ${timeDiff}`);
+}
+
+const afterAll = process.memoryUsage().heapUsed
+console.log(`total: ${Math.round((afterAll - beforeAll) / 1024)} MB`);
